61a08c7d410034b430a5c3031a8d80590ac11992
fourki <fourki2002@gmail.com>
2023-07-05 17:28:53 +0700

    update layout dan halaman

--- resources/views/dashboard/halaman/index.blade.php
+++ resources/views/dashboard/halaman/index.blade.php
@@ -1,1 +1,8 @@
-Hi
+@extends('dashboard.layout')
+
+@section('konten')
+    <p class="card-title">Halaman</p>
+    <div class="pb"><a href="" class="btn btn-primary">+ Tambah Halaman</a></div>
+    <div class="table-responsive">
+    </div>
+@endsection
