107375b4a1fe76a3c0d7c5298543b84fe42bd5a1
fourki <fourki2002@gmail.com>
2023-07-06 19:23:41 +0700

    update route skill controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\authController;
 use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\skillController;
 use App\Http\Controllers\halamanController;
 use App\Http\Controllers\educationController;
 use App\Http\Controllers\experienceController;
@@ -37,5 +38,6 @@ Route::prefix('dashboard')->middleware('auth')->group(
         Route::resource('halaman', halamanController::class);
         Route::resource('experience', experienceController::class);
         Route::resource('education', educationController::class);
+        Route::get('skill', [skillController::class,"index"]);
     }
 );
