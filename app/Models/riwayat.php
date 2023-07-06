1191109f3cf77c54de712588f7ead76b4a7c624b
fourki <fourki2002@gmail.com>
2023-07-06 12:11:23 +0700

    experience controller

--- app/Models/riwayat.php
+++ app/Models/riwayat.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class riwayat extends Model
 {
     use HasFactory;
+    protected $table = "riwayat";
+    protected $fillable = ['judul', 'tipe', 'tgl_mulai', 'tgl_akhir', 'infor1', 'info2', 'info3', 'isi'];
 }
