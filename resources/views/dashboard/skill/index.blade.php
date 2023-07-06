0caad98319f53f1211578f6f3c1c25bbaa41ef33
fourki <fourki2002@gmail.com>
2023-07-06 19:27:00 +0700

    2 fungsi blade

--- resources/views/dashboard/skill/index.blade.php
+++ resources/views/dashboard/skill/index.blade.php
@@ -1,19 +1,16 @@
 @extends('dashboard.layout')
 
 @section('konten')
-    <div class="pb-3"><a href="{{ route('halaman.index') }}" class="btn btn-secondary">
-            Kembali</a>
-    </div>
-    <form action="{{ route('halaman.store') }}" method="POST">
+    <form action="{{ route('skill.store') }}" method="POST">
         @csrf
         <div class="mb-3">
             <label for="judul" class="form-label">PROGRAMMING LANGUAGES & TOOLS</label>
-            <input type="text" class="form-control form-control-sm" name="judul" id="judul" aria-describedby="helpId"
-                placeholder="judul" value="{{ Session::get('judul') }}">
+            <input type="text" class="form-control form-control-sm" name="_language" id="judul" aria-describedby="helpId"
+                placeholder="Programming Language & Tools" value="">
         </div>
         <div class="mb-3">
             <label for="isi" class="form-label">WORKFLOW</label>
-            <textarea class="form-control summernote" rows="5" name="isi">{{ Session::get('isi') }}</textarea>
+            <textarea class="form-control summernote" rows="5" name="_workflow"></textarea>
         </div>
         <button class="btn btn-primary" name="simpan" type="submit">SIMPAN</button>
 
