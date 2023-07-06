ea9c7f3b18b9538d8df2d46b90b88d9c09b3d0e6
fourki <fourki2002@gmail.com>
2023-07-06 19:42:06 +0700

    update data skill

--- app/Http/Controllers/skillController.php
+++ app/Http/Controllers/skillController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\metadata;
 use Illuminate\Http\Request;
 
 class skillController extends Controller
@@ -20,6 +21,11 @@ class skillController extends Controller
                 '_language.required' => 'Silahkan masukkan bahasa Pemrograman yang anda kuasai',
                 '_workflow.required' => 'Silahkan masukkan workflow yang anda kuasai',
            ]);
+
+           metadata::updateOrCreate(['meta_key' => '_language'], ['meta_value' => $request->_language]);
+           metadata::updateOrCreate(['meta_key' => '_workflow'], ['meta_value' => $request->_workflow]);
+        
+           return redirect()->route('skill.index')->with('success', 'Anda Berhasil melakukan update data skill.');
         }
     }
 }
